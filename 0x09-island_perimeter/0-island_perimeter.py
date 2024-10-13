2c92b6de79bb88591f0655dc3989721932c44cff
Tgithinji <t.muraguri420@gmail.com>
2024-10-13 14:41:54 -0400

    Island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!usr/bin/env python3
+"""island_perimetermodule
+"""
+
+def island_perimeter(grid):
+    """Island perimeter
+    """
+    rows = len(grid)
+    columns = len(grid[0])
+
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(columns):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == columns-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter
